c2c809a1e278be75211fb83d3ba7975f77f0a309
egthomas <evan.g.thomas@dartmouth.edu>
2025-06-16 15:42:34 -0400

    Modifying SetInactiveHandler in usrp_server for better debugging and error handling around channel removal

--- usrp_server/usrp_server.py
+++ usrp_server/usrp_server.py
@@ -3843,11 +3843,14 @@ class RadarChannelHandler:
             try:
                RHM.active_channels.remove(channelObject)
             except:
-               RHM.logger.info('ROS:SET_INACTIVE failed to remove channel {} from RHM.active_channels'.format(RHM.channels.index(channelObject)))
+               RHM.logger.info('ROS:SET_INACTIVE failed to remove channel from RHM.active_channels')
 
         if channelObject in RHM.channels:
             RHM.logger.info('ROS:SET_INACTIVE removing channel {} from HardwareManager'.format(RHM.channels.index(channelObject)))
-            RHM.channels.remove(channelObject)
+            try:
+                RHM.channels.remove(channelObject)
+            except:
+                RHM.logger.info('ROS:SET_INACTIVE failed to remove channel from HardwareManager')
 
             RHM.nRegisteredChannels -= 1
             if RHM.nRegisteredChannels == 0:
